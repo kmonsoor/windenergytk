f48b767d404d6a7d059b3d00dda762f69827cb5a
AKoumjian <AKoumjian@gmail.com>
2010-02-27 19:57:51 +0000

    first complete myklestad draft

--- windenergytk/testing_myklestad.py
+++ windenergytk/testing_myklestad.py
@@ -10,38 +10,13 @@
 ################################################################################
 import numpy as np
 
-def influence(n_sections, sec_lengths, ei):
-    """Used to obtain the influence coefficients from the length and stiffness of each section
-    
-    INPUT
-    n_sections: (int) number of sections
-    sec_lengths: (array-like) length of each section
-    ei: (array-like) stiffness of each section
-    
-    OUTPUT
-    slope_from_moment: (array-like) 
-    slope_from_shear: (array-like) slope at each point caused by shear force
-    deflection_shear: (array-like) deflection at each section caused by shear
-    
-    """
-    slope_from_moment = []
-    slope_from_shear = []
-    deflection_shear = []
-    
-    for i in range(n_sections):
-        slope_from_moment[i] = sec_lengths[i] / ei[i]
-        slope_from_shear[i] = slope_from_moment[i] * sec_lengths[i] / 2.
-        deflection_shear[i] = slope_from_shear[i] * sec_lengths[i] * 2. / 3.
-    
-    return slope_from_moment, slope_from_shear, deflection_shear
-
 def myklestad_beam_vibrations(sec_lengths, sec_masses, e_i, density, 
                               rot_velocity, freq_start, freq_final, freq_step):
     """Estimate the natural freq of a nonuniform vibrating cantilevered beam.
     
     INPUT
-    sec_lengths: (array-like) length of each section
-    sec_mass: (array-like) mass of each section
+    sec_lengths: (array-like) length of each section starting at free end
+    sec_masses: (array-like) mass of each section
     e_i: (array-like) structural stiffness, young's modulus * area inertia
     density: (float) density of material
     rot_velocity: (float) rotational velocity in rad/s
@@ -67,17 +42,31 @@ def myklestad_beam_vibrations(sec_lengths, sec_masses, e_i, density,
     # Input rotational velocity as rad/s instead of rpm (use external function
     # to sanitize input)
     
-    # Create empty arrays
-    slope = np.empty([2,len(sec_lengths)])
-    deflection = np.empty([2,len(sec_lengths)])
-    f_cent = np.empty([2,len(sec_lengths)])
-    vert_shear = np.empty([2,len(sec_lengths)])
-    bend_moment = np.empty([2,len(sec_lengths)])
+    # 1. Setup prior to main algorithm
+    
+    # calculate number of stations
+    n_stations = len(sec_lengths)
+    
+    # Create array of total distance from rotation axis for each mass
+    dist_from_axis = np.empty(n_stations)
+    dist_from_axis[0] = sec_lengths[0] / 2.
+    for i in range(1, n_stations):
+        dist_from_axis[i] = dist_from_axis[i-1] + \
+        sec_lengths[i]
+    
+    # Create other empty arrays
+    nat_frequencies = []
+    slope = np.empty([2,n_stations])
+    deflection = np.empty([2,n_stations])
+    f_cent = np.empty([2,n_stations])
+    vert_shear = np.empty([2,n_stations])
+    bend_moment = np.empty([2,n_stations])
     
     # Initial conditions for two components
     initial_slope = [0, 1]
     initial_deflection = [1, 0]
     
+    composite_deflection = 1
     
     # Iterate through range of possible natural frequencies
     for freq in range(freq_start, freq_final, freq_step):
@@ -90,7 +79,20 @@ def myklestad_beam_vibrations(sec_lengths, sec_masses, e_i, density,
             bend_moment[i][0] = 0
             
             # Calculate forces/deflections at each station
-            for n in range(len(sec_lengths)):
+            for n in range(1, n_stations):
+                f_cent[i][n] = f_cent[i][n-1] + freq**2 * sec_masses[n-1] * dist_from_axis[n-1]
                 
-    
-    return 0
+                vert_shear[i][n] = vert_shear[i][n-1] - freq**2 * sec_masses[n-1] * deflection[n-1] - f_cent[i][n] * slope[i][n-1]
+                
+                bend_moment[i][n] = (bend_moment[i][n-1] - vert_shear[i][n] * (sec_lengths[n-1] - f_cent[i][n] * (sec_lengths[n-1]**3/(3*e_i[n-1]))) + slope[i][n-1]*sec_lengths[n-1]*f_cent[i][n]) / (1 - f_cent[i][n] * sec_lengths[n-1]**2 / (2 * e_i[n-1]))
+                
+                slope[i][n] = slope[i][n-1] + bend_moment[i][n] * (sec_lengths[n-1]/e_i[n-1]) + vert_shear[i][n] * (sec_lengths[n-1]**2/ (2 * e_i[n-1]))
+                
+                deflection[i][n] = deflection[i][n-1] + slope[i][n-1] * sec_lengths[n-1] + bend_moment[i][n] * (sec_lengths[n-1]**2 / (2 * e_i[n-1])) + vert_shear[i][n] * (sec_lengths[n-1]**3 / (3 * e_i[n-1]))
+        
+        old_composite_deflection = composite_deflection
+        composite_deflection = deflection[0][-1] - deflection[1][-1] * (slope[0][-1]/slope[1][-1])
+        if np.sign(old_composite_deflection) != np.sign(composite_deflection):
+            nat_frequencies.append(freq)
+        
+    return nat_frequencies
